90e3ce8c581840a89002ab2d02107aeaeb53de9a
kr8n3r <jani@kraner.dev>
2019-07-08 16:14:56 +0100

    Store policies in session data

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -16,9 +16,138 @@ Example usage:
 ============================================================================
 
 */
-
+module.exports.policyOwners = []
+module.exports.areas = [
+  {
+    name: 'Grand Challenges'
+  },
+  {
+    name: 'Sector Deals'
+  },
+  {
+    name: 'Ideas'
+  },
+  {
+    name: 'People'
+  },
+  {
+    name: 'Infrastructure'
+  },
+  {
+    name: 'Business Environment'
+  },
+  {
+    name: 'Places'
+  },
+  {
+    name: 'IS Council'
+  }
+]
 module.exports = {
 
-  // Insert values here
+  // current policies
+  'policies': [
+    {
+      title: 'Artificial Intelligence and Data',
+      area: module.exports.areas[0].name
+    },
+    {
+      title: 'Clean Growth',
+      area: module.exports.areas[0].name
+    },
+    {
+      title: 'Future of Mobility',
+      area: module.exports.areas[0].name
+    },
+    {
+      title: 'Ageing Society',
+      area: module.exports.areas[0].name
+    },
+    {
+      title: 'Published sector deals',
+      area: module.exports.areas[1].name
+    },
+    {
+      title: 'Sector deals under negotiation',
+      area: module.exports.areas[1].name
+    },
+    {
+      title: 'Emerging sector deals',
+      area: module.exports.areas[1].name
+    },
+    {
+      title: 'Increase R&D investment to 2.4% of GDP',
+      area: module.exports.areas[2].name
+    },
+    {
+      title: 'Industrial Strategy Challenge Fund',
+      area: module.exports.areas[2].name
+    },
+    {
+      title: 'Commercialisation (beyond ISCF)',
+      area: module.exports.areas[2].name
+    },
+    {
+      title: 'Strength in Places Fund',
+      area: module.exports.areas[2].name
+    },
+    {
+      title: 'Technical Skills',
+      area: module.exports.areas[3].name
+    },
+    {
+      title: 'STEM',
+      area: module.exports.areas[3].name
+    },
+    {
+      title: ' National Retraining Scheme',
+      area: module.exports.areas[3].name
+    },
+    {
+      title: 'Transforming Cities Fund',
+      area: module.exports.areas[4].name
+    },
+    {
+      title: 'NPIF',
+      area: module.exports.areas[4].name
+    },
+    {
+      title: 'Digital Infrastructure',
+      area: module.exports.areas[4].name
+    },
+    {
+      title: 'Access to Finance',
+      area: module.exports.areas[5].name
+    },
+    {
+      title: 'Long tail review',
+      area: module.exports.areas[5].name
+    },
+    {
+      title: 'Export Strategy',
+      area: module.exports.areas[5].name
+    },
+    {
+      title: 'Local Industrial Strategies',
+      area: module.exports.areas[6].name
+    },
+    {
+      title: 'UK Shared Prosperity Fund',
+      area: module.exports.areas[6].name
+    },
+    {
+      title: 'Regional Skills',
+      area: module.exports.areas[6].name
+    },
+    {
+      title: 'Public Body Relocation/ Places for Growth',
+      area: module.exports.areas[6].name
+    },
+    {
+      title: 'Formation of the Council',
+      area: module.exports.areas[7].name
+    }
+  ],
+  'policyOwners': []
 
 }
